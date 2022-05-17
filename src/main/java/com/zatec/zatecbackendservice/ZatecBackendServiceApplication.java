d3bd43cb3c7ebb30c2442b326f571814e918e7b8
ahmedopeyemikabiru <opeyemi.kabiru@yahoo.com>
2022-05-17 17:46:25 +0100

    initialize project with dependencies

---
+++ src/main/java/com/zatec/zatecbackendservice/ZatecBackendServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zatec.zatecbackendservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZatecBackendServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZatecBackendServiceApplication.class, args);
+    }
+
+}
